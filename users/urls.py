c828b31ff6c46407399fa726532effc7e46c6f2e
Shaun <nhlamulosmashaba@gmail.com>
2024-11-10 14:12:41 +0200

    adjudted urls

--- users/urls.py
+++ users/urls.py
@@ -4,8 +4,8 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('user/', get_current_user, name='get-user'),
-    path('register/', register_view, name='register'),
-    path('login/', login_view, name='login'),
+    path('auth/register/', register_view, name='register'),
+    path('auth/login/', login_view, name='login'),
     path('users/<int:pk>/', get_user_by_id, name='user-detail'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
